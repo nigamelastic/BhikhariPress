bf549694b20999870f12a5bf0e177dbd9093b5d0
root <root@kali.k>
2019-01-16 01:57:07 +0100

    added searching functionality and pretty much everything else

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # BhikhariPress
+
+## Desription: ##
+
+The JS code uses cypress to login to wg-gesuct and apply/ send messages to all the WG adds on a certain city, in the given prices range.
+
+## Pre-requisites: ##
+
+NPM 5.8 or higher
+node 8.11 or higher
+
+## Usage: ##
+
+1) Replace username, password, city and max price range to yours
+
+2) To install the packages run :
+```bash
+npm install
+```
+3) To run the script :
+```bash
+npm run jeher
+```
--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -1,3 +1,7 @@
+var username ="nigamelastic@gmail.com"//"your username"
+var password = "ben10alienforce"//"your password"
+var city = "Wien"
+var maxRent= "500"
 describe('Sending ', function() {
     context('Running Before Class', function() {
         before(function() {
@@ -8,6 +12,35 @@ describe('Sending ', function() {
             //cy.log("--------->"+cy.title);
             //expect(cy.title).to.equal("true")
           }) 
+          it('logs in and searhes for houses',function(){
+
+            cy.get('#headbar_wrapper > div.col-sm-8 > a:nth-child(2)').click();
+            cy.get('#login_email_username').type(username)
+            cy.get('#login_password').type(password)
+            cy.get('#login_submit').click(); 
+          })
+
+          
+          it('it searches for flats in your city',function(){
+            cy.get('#autocompinp').type(city);
+            //cy.get('#autocompinp').type("enter")
+            //cy.get('#autocompinp').type()
+            //cy.get('#rMax').type(maxRent);
+            cy.get('#search_button').click()
+          })
+
+          it('it searches for prices range',function(){
+            
+            //cy.get('#autocompinp').type("enter")
+            //cy.get('#autocompinp').type()
+            cy.get('#rMax').type(maxRent);
+            cy.get('#offer_filter_form > div:nth-child(9) > div.col-sm-12.col-md-4 > button.filter_submit_button.btn.btn-info.btn-md.hidden-xs').click()
+            cy.get('h4 > a.detailansicht').each(($el, index, $list) =>{
+              cy.wrap($el).click();
+              
+  
+            })
+          })
     });
 
     
